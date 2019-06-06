331c744bba6d87d9ec452e1c550b49bfa7242653
bombom70 <khamdamov_1996@mail.ru>
2019-06-06 13:03:37 +0500

    add argements in gendiff

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -4,6 +4,7 @@ import commander from 'commander';
 const program = require('commander');
 
 program
+  .arguments('<firstConfig> <secondConfig>')
   .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'Output format')
