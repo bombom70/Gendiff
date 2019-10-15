8aee704cf250ce6764307e7c20b278e13d72678b
bombom70 <khamdamov_1996@mail.ru>
2019-10-16 00:13:10 +0500

    test commit

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,19 +1,23 @@
-import diff from '../src';
+import fs from 'fs';
 import path from 'path';
+import diff from '../src';
 
 test('diff JSON file', () => {
+  const f1 = fs.readFileSync(path.resolve(__dirname, '__fixtures__/before.json'));
+  const f2 = fs.readFileSync(path.resolve(__dirname, '__fixtures__/after.json'));
+  const result = fs.readFileSync(`${__dirname}/__fixtures__/resultJSON.txt`, 'utf-8');
+  const expected = diff(f1, f2);
+  console.log(fs.readFileSync(f1, 'utf-8'));
+  console.log(f2);
+  console.log(typeof result === 'string');
+  console.log(typeof expected === 'string');
 
-  const f1 = path.resolve(__dirname, '__fixtures__/before.json');
-  const f2 = path.resolve(__dirname, '__fixtures__/after.json');
-
-  expect(diff(f1, f2)).toBe(`{\n\t  host: hexlet.io\n\t+ timeout: 20\n\t- timeout: 50\n\t- proxy: 123.234.53.22\n\t- follow: false\n\t+ verbose: true\n}`); 
+  expect(diff(f1, f2)).toBe(result);
 });
 
 test('diff yaml file', () => {
-
   const f1 = path.resolve(__dirname, '__fixtures__/before.yaml');
   const f2 = path.resolve(__dirname, '__fixtures__/after.yaml');
-
   expect(diff(f1, f2)).toBe(`{\n\t  host: hexlet.io\n\t+ timeout: 20\n\t- timeout: 50\n\t- proxy: 123.234.53.22\n\t- follow: false\n\t+ verbose: true\n}`); 
 });
 
