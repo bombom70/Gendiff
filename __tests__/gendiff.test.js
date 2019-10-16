acd587982cd88b748bca169f7d18a9962f728757
bombom70 <khamdamov_1996@mail.ru>
2019-10-16 08:07:19 +0500

    test

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,12 +1,11 @@
 import fs from 'fs';
-import diff from '../src';
-import parser from '../src/parser';
+import { render, parse } from '../src';
 
 test('diff JSON', () => {
-  const before = parser(fs.readFileSync(`${__dirname}/__fixtures__/before2.json`, 'utf-8'));
-  const after = parser(fs.readFileSync(`${__dirname}/__fixtures__/after2.json`, 'utf-8'));
+  const before = JSON.parse(fs.readFileSync(`${__dirname}/__fixtures__/before2.json`, 'utf-8'));
+  const after = JSON.parse(fs.readFileSync(`${__dirname}/__fixtures__/after2.json`, 'utf-8'));
   const expected = fs.readFileSync(`${__dirname}/__fixtures__/resultJSON.txt`, 'utf-8');
-  const result = diff(before, after);
+  const result = parse(render(before, after));
 
   expect(result).toBe(expected);
 });
