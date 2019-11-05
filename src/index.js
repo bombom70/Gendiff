a7b882b6a654cb49935d28b91e218549d58c635c
bombom70 <khamdamov_1996@mail.ru>
2019-11-05 20:28:24 +0500

    change name variable

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import parse from './parser';
 import selectFormta from './formatters/index';
 import buildAst from './buildAst';
 
-export default (pathToFile1, pathToFile2, format) => {
-  const before = parse(path.extname(`${__dirname}${pathToFile1}`), pathToFile1);
-  const after = parse(path.extname(`${__dirname}${pathToFile2}`), pathToFile2);
+export default (filepath1, filepath2, format) => {
+  const before = parse(path.extname(`${__dirname}${filepath1}`), filepath1);
+  const after = parse(path.extname(`${__dirname}${filepath2}`), filepath2);
   const ast = buildAst(before, after);
   return selectFormta(ast, format);
 };
