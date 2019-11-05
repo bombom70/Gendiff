ac5d10db316fc05afa4da6acfc713d1b16537941
bombom70 <khamdamov_1996@mail.ru>
2019-11-05 20:22:12 +0500

    change name variable

--- src/parser.js
+++ src/parser.js
@@ -2,12 +2,12 @@ import fs from 'fs';
 import ini from 'ini';
 import yaml from 'js-yaml';
 
-const reeadFile = pathToFile => fs.readFileSync(pathToFile, 'utf-8');
+const reeadFile = filepath => fs.readFileSync(filepath, 'utf-8');
 
 const mapping = {
-  '.json': f => JSON.parse(reeadFile(f)),
-  '.yaml': f => yaml.safeLoad(reeadFile(f)),
-  '.ini': f => ini.parse(reeadFile(f)),
+  '.json': filepath => JSON.parse(reeadFile(filepath)),
+  '.yaml': filepath => yaml.safeLoad(reeadFile(filepath)),
+  '.ini': filepath => ini.parse(reeadFile(filepath)),
 };
 
-export default (format, pathToFile) => mapping[format](pathToFile);
+export default (format, filepath) => mapping[format](filepath);
