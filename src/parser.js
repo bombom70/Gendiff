0bc07329bff66259c79da7cf933d1fc4a5a141bb
bombom70 <khamdamov_1996@mail.ru>
2019-11-05 20:19:52 +0500

    change funciton readFile

--- src/parser.js
+++ src/parser.js
@@ -2,10 +2,7 @@ import fs from 'fs';
 import ini from 'ini';
 import yaml from 'js-yaml';
 
-const reeadFile = (pathToFile) => {
-  console.log(pathToFile);
-  return fs.readFileSync(pathToFile, 'utf-8');
-};
+const reeadFile = pathToFile => fs.readFileSync(pathToFile, 'utf-8');
 
 const mapping = {
   '.json': f => JSON.parse(reeadFile(f)),
