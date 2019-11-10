16fca2a99f9da26a9c98aecea89b8fef966dfdc3
bombom70 <khamdamov_1996@mail.ru>
2019-11-10 11:43:12 +0500

    remove funcion name

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import path from 'path';
 import fs from 'fs';
 import parse from './parser';
-import render from './formatters';
-import buildAst from './buildAst';
+import selectFormta from './formatters';
+import render from './buildAst';
 
 export default (filepath1, filepath2, format) => {
   const before = parse(path.extname(`${__dirname}${filepath1}`), fs.readFileSync(filepath1, 'utf-8'));
   const after = parse(path.extname(`${__dirname}${filepath2}`), fs.readFileSync(filepath2, 'utf-8'));
-  const ast = buildAst(before, after);
-  return render(ast, format);
+  const ast = render(before, after);
+  return selectFormta(ast, format);
 };
