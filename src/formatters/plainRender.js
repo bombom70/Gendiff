dab59e8c6bee81185e3cf9b914eb1573541713e5
bombom70 <khamdamov_1996@mail.ru>
2019-11-22 20:52:53 +0500

    change logic program

--- src/formatters/plainRender.js
+++ src/formatters/plainRender.js
@@ -1,25 +1,28 @@
 import _ from 'lodash/fp';
 
+const getValue = value => (_.isObject(value) ? '[complex value]' : value);
+
+const strBuild = (valueBefore, valueAfter) => {
+  if (_.isObject(valueAfter)) {
+    return ': [complex value]';
+  }
+  return `. From ${getValue(valueBefore)} to ${getValue(valueAfter)}`;
+};
+
 const plainRender = (ast, acc = '') => {
   const result = ast.map((node) => {
-    const getValue = _.isObject(node.value) ? '[complex value]' : node.value;
-    const getValueFromObject = _.isObject(node.valueAfter) || _.isObject(node.valueAfter) ? '[complex value]' : node.valueAfter;
-    if (node.type === 'changeInside') {
-      return plainRender(node.children, `${acc}${node.name}.`);
+    switch (node.type) {
+      case 'changeInside':
+        return plainRender(node.children, `${acc}${node.name}.`);
+      case 'added':
+        return `Property '${acc}${node.name}' was added with value: ${getValue(node.value)}`;
+      case 'changed':
+        return `Property '${acc}${node.name}' was updated${strBuild(node.valueBefore, node.valueAfter)}`;
+      case 'deleted':
+        return `Property '${acc}${node.name}' was removed`;
+      default:
+        return `Property '${acc}${node.name}' not changed`;
     }
-    if (node.type === 'added') {
-      return `Property '${acc}${node.name}' was added with value: ${getValue}`;
-    }
-    if (node.type === 'changed') {
-      if (_.isObject(node.valueBefore) || _.isObject(node.valueAfter)) {
-        return `Property '${acc}${node.name}' was added with value: ${getValueFromObject}`;
-      }
-      return `Property '${acc}${node.name}' was updated. From ${node.valueBefore} to ${node.valueAfter}`;
-    }
-    if (node.type === 'deleted') {
-      return `Property '${acc}${node.name}' was removed`;
-    }
-    return `Property '${acc}${node.name}' not changed`;
   });
   return result.join('\n');
 };
--- src/formatters/treeReder.js
+++ src/formatters/treeReder.js
@@ -1,26 +1,20 @@
 import stringify from './stringify';
 
 const repeat = num => ' '.repeat(num);
-const createSpaceEnd = (num) => {
-  switch (num) {
-    case 2:
-      return repeat(8);
-    case 1:
-      return repeat(4);
-    default:
-      return repeat(0);
-  }
-};
-const createSpace = (depth) => {
+export const createSpace = (depth) => {
   switch (depth) {
     case 0:
       return repeat(2);
     case 1:
-      return repeat(6);
+      return repeat(4);
     case 2:
+      return repeat(6);
+    case 3:
+      return repeat(8);
+    case 4:
       return repeat(10);
     default:
-      return repeat(10);
+      return repeat(0);
   }
 };
 const selectSign = (type) => {
@@ -38,18 +32,20 @@ const treeRender = (ast, depth = 0) => {
   const result = ast.map((node) => {
     switch (node.type) {
       case 'changeInside':
-        return `${createSpace(depth)}${selectSign(node.type)} ${node.name}: ${treeRender(node.children, depth + 1)}`;
+        return `${createSpace(depth)}${selectSign(node.type)} ${node.name}: ${treeRender(node.children, depth + 2)}`;
       case 'added':
         return `${createSpace(depth)}${selectSign(node.type)} ${node.name}: ${stringify(node.value, depth)}`;
       case 'changed':
-        return `${createSpace(depth)}- ${node.name}: ${stringify(node.valueBefore)}\n${createSpace(depth)}+ ${node.name}: ${stringify(node.valueAfter)}`;
+        return `${createSpace(depth)}- ${node.name}: ${stringify(node.valueBefore, depth)}\n${createSpace(depth)}+ ${node.name}: ${stringify(node.valueAfter, depth)}`;
       case 'unchanged':
-        return `${createSpace(depth)}${selectSign(node.type)} ${node.name}: ${node.value}`;
-      default:
         return `${createSpace(depth)}${selectSign(node.type)} ${node.name}: ${stringify(node.value, depth)}`;
+      case 'deleted':
+        return `${createSpace(depth)}${selectSign(node.type)} ${node.name}: ${stringify(node.value, depth)}`;
+      default:
     }
+    return node;
   });
-  return `{\n${result.join('\n')}\n${createSpaceEnd(depth)}}`;
+  return `{\n${result.join('\n')}\n${createSpace(depth - 1)}}`;
 };
 
 export default treeRender;
